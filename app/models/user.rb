814384b5563d8067fb5eaa9fc47d077dad23e1e2
Joseph Huang <huangx5j@gmail.com>
2017-04-11 14:32:16 -0700

    Update user.rb
    refactor bcrypt methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,22 @@
+# require 'bcrypt'
 class User < ActiveRecord::Base
+  include BCrypt
 
   validates :user_name, presence: true
   validates :email, uniqueness: true
   validates :password_hash, presence: true
 
   def password
-    @password ||= BCrypt::Password.new(password_hash)
+    @password ||= Password.new(password_hash)
   end
 
   def password=(new_password)
-    @password = BCrypt::Password.create(new_password)
+    @password = Password.create(new_password)
     self.password_hash = @password
   end
 
-  def self.authenticate(email, password)
-  	valid_user = User.find_by(email: email)
-  	valid_user.password == password ? valid_user : nil
+  def authenticate(email_input, password_input)
+    self.email == email_input && self.password == password_input
   end
 
 end
