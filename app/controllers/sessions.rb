7588c30866d40661e829f58af40e1b32da572a1d
Joseph Huang <huangx5j@gmail.com>
2017-04-11 14:28:34 -0700

    Update sessions.rb
    refactor sessions

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,44 +1,22 @@
-get '/sessions/login' do
-	erb :'sessions/new'
-end
-
-post '/login' do
-	@user = User.authenticate(params[:user][:email], params[:user][:password])
-	if validation_fail
-		erb :'/sessions/new'
-	elsif @user
-		login
-		redirect "/users/#{@user.id}"
-	end
-end
-
-delete '/logout' do
-	logout
-	redirect '/'
-end
-
-
-# different routes where you don't need login
-
 # login form
-get '/sessions' do
+get '/sessions/new' do
 	erb :'/sessions/new'
 end
 
 # login action
 post '/sessions' do
-	@user = User.find_by(email: params[:email])
-	if @user && @user.password == params[:password]
-		session[:id] = @user.id
-		redirect '/users'
-	else
-		
-	end
-
+  @user = User.authenticate(params[:user][:email], params[:user][:password])
+  if @user
+    login
+    redirect "/users/#{@user.id}"
+  else
+    @errors = ["Email or Password not found"]
+    erb :'/sessions/new'
+  end
 end
 
 #logout action
 delete '/sessions' do
-	session[:id] = nil
+	logout
 	redirect '/'
 end
